f546f22e7765530272ec2a235559e580a8f2f95a
Ashenafi <131515289+Ashenafi-Godana@users.noreply.github.com>
2024-06-09 00:08:32 +0300

    Time: 14 ms (78.94%), Space: 11.5 MB (98.34%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -4,19 +4,15 @@ class Solution(object):
         :type word: str
         :rtype: int
         """
-        letters = []
-        output = 0
-        checked = []
-        for letter in word:
-            if letter.lower() not in checked and (letter.isupper() and letter.lower() in letters):
-                output = output + 1
-                checked.append(letter.lower())
-            if letter not in checked and (letter.islower() and letter.upper() in letters):
-                output = output + 1
-                checked.append(letter)
-            elif letter not in letters:
-                letters.append(letter)
-        print(letters)
-        return output
+        lower = set()
+        upper = set()
+
+        for c in word:
+            if c.isupper():
+                upper.add(c.lower())
+            elif c.islower():
+                lower.add(c.lower())
+
+        return len(lower & upper)
 
         
