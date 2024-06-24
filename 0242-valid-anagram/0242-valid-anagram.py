5ff550694ff87be5dba735b43d8f08e7ed661a8c
Ashenafi <131515289+Ashenafi-Godana@users.noreply.github.com>
2024-06-24 20:06:51 +0300

    Time: 47 ms (68.12%), Space: 16.8 MB (81.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,14 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        is_anagram = True
-        if (len(s) != len(t)):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
             return False
-
-        for i in range(len(s)):
-            if s[i] not in t:
-                is_anagram = False
-                break
-            idx = t.index(s[i])
-            t = t[0:idx] + t[idx+1:]
-
-        return is_anagram
         
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
