162a1126f7bb7c67395be1d9ed95f70896d60fcd
Ashenafi <131515289+Ashenafi-Godana@users.noreply.github.com>
2024-07-19 07:51:39 +0300

    Time: 92 ms (39.48%), Space: 12.4 MB (61.01%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,15 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
         l = 0
         r = len(numbers)-1
         res = []
-        while l < r:
-            currSum = numbers[l] + numbers[r]
-            if currSum > target:
+        
+        while l<r:
+            if numbers[l] + numbers[r] > target:
                 r-=1
-            elif currSum < target:
+            elif numbers[l] + numbers[r] < target:
                 l+=1
             else:
                 return [l+1, r+1]
