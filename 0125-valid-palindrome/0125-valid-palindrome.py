7eb3cad2981701e91d9f70d076b741b6a0469dd9
Ashenafi <131515289+Ashenafi-Godana@users.noreply.github.com>
2024-07-19 10:30:27 +0300

    Time: 40 ms (81.68%), Space: 17.8 MB (30.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,12 +5,14 @@ class Solution(object):
         :rtype: bool
         """
         s = ''.join(filter(str.isalnum, s.lower()))
-        l =0
-        r = len(s)-1
+
+        l = 0
+        r = len(s) - 1
         
-        while l<r:
+        while l < r:
             if s[l] != s[r]:
                 return False
             l+=1
             r-=1
         return True
+        
