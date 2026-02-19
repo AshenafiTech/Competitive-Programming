a608aa83740fd1c8ca9749ee4babcd0bb2fb3250
Ashenafi G. Kumbi <131515289+AshenafiTech@users.noreply.github.com>
2026-02-19 21:33:12 +0300

    Time: 115 ms (54.95%), Space: 56.9 MB (64.63%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -0,0 +1,33 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.nums = []
+        self.pos = {}
+
+    def insert(self, val: int) -> bool:
+        if val in self.pos:
+            return False
+        
+        self.pos[val] = len(self.nums)
+        self.nums.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.pos:
+            return False
+        
+        index = self.pos[val]
+        last = self.nums[-1]
+
+        # swap
+        self.nums[index] = last
+        self.pos[last] = index
+
+        # remove
+        self.nums.pop()
+        del self.pos[val]
+
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.nums)
